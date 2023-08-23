5b040718dfb9f085919efe189f40a502d9b44a65
Rady Haggag <radyhaggag11@gmail.com>
2023-08-23 23:31:50 +0300

    Complete task controller methods

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,24 +1,68 @@
 const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
-  res.send("All tasks");
+const getAllTasks = async (req, res) => {
+  try {
+    let tasks = await Task.find({});
+    res.status(200).send({ tasks });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
-const getTask = (req, res) => {
-  res.send({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    let { id: taskId } = req.params;
+    let task = await Task.findOne({ _id: taskId });
+    if (!task) {
+      return res.status(404).send({ msg: `Task not found wit id : ${taskId}` });
+    }
+    res.status(200).send({ task });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
-const updateTask = (req, res) => {
-  res.send("Update task");
+const updateTask = async (req, res) => {
+  try {
+    let { id: taskId } = req.params;
+    let { completed, name } = req.body;
+    let task = await Task.findOne({ _id: taskId });
+    if (!task) {
+      return res.status(404).send({ msg: `Task not found wit id : ${taskId}` });
+    }
+    await task.updateOne({
+      completed: completed,
+      name: name,
+    });
+    if (completed) task.completed = completed;
+    if (name) task.name = name;
+    res.status(200).send({ msg: "Task Updated Successfully.", task: task });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("Delete task");
+const deleteTask = async (req, res) => {
+  try {
+    let { id: taskId } = req.params;
+    let task = await Task.findOne({ _id: taskId });
+    if (!task) {
+      return res.status(404).send({ msg: `Task not found wit id : ${taskId}` });
+    }
+    await task.deleteOne();
+    res.status(200).send({ msg: "Task Deleted Successfully." });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 module.exports = {
--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "Must Provide a name for the task"],
+    trim: true,
+    maxLength: [50, "The Max Length is 50"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
