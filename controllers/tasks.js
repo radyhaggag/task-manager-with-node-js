e005505b2c7dc3718a3f4ba90627956e50191ade
Rady Haggag <radyhaggag11@gmail.com>
2023-08-23 05:26:16 +0300

    first commit

---
+++ controllers/tasks.js
@@ -0,0 +1,30 @@
+const Task = require("../models/Task");
+
+const getAllTasks = (req, res) => {
+  res.send("All tasks");
+};
+
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+};
+
+const getTask = (req, res) => {
+  res.send({ id: req.params.id });
+};
+
+const updateTask = (req, res) => {
+  res.send("Update task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("Delete task");
+};
+
+module.exports = {
+  getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+};
